9a7603f03d03286e48f46de23d1a952990c76fcc
Phong Vo <phongvxk38@gmail.com>
2025-02-16 16:53:14 +0700

    feat: Update BASE_URL for cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000", // Default URL for tests
+    baseUrl: process.env.BASE_URL || "http://localhost:3000", // Default URL for tests
     supportFile: "cypress/support/e2e.ts", // Support file path
     viewportWidth: 1280,
     viewportHeight: 720,
